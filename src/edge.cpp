d39cd621e6762f19c3f6b67e852e44cdd15f4b14
adamisetty <ankithad@illinois.edu>
2020-05-05 13:02:17 -0500

    Renames PROPOSAL.md and fixes indentation to reflect correct C++ style

--- src/edge.cpp
+++ src/edge.cpp
@@ -6,27 +6,27 @@ namespace myapp {
 const float32 small_distance = 0.002;
 const float32 density = 0.0f;
 
-  myapp::Edge::Edge() {}
+myapp::Edge::Edge() {}
 
-  void myapp::Edge::initialize(b2World *my_world, b2Vec2 start, b2Vec2 end) {
-    b2BodyDef sample_edge;
-    //creates body centered at the midpoint of the line segment given
-    sample_edge.position.Set((start.x + end.x)/2, (start.y + end.y)/2);
-    edgeBody = my_world->CreateBody(&sample_edge);
+void myapp::Edge::initialize(b2World *my_world, b2Vec2 start, b2Vec2 end) {
+  b2BodyDef sample_edge;
+  //creates body centered at the midpoint of the line segment given
+  sample_edge.position.Set((start.x + end.x)/2, (start.y + end.y)/2);
+  edgeBody = my_world->CreateBody(&sample_edge);
 
-    //must transform the shape to be at the same angle as the drawn line
-    float32 angle = atan((start.y - end.y)/ (start.x - end.x));
-    edgeBody->SetTransform(edgeBody->GetPosition(), angle);
+  //must transform the shape to be at the same angle as the drawn line
+  float32 angle = atan((start.y - end.y)/ (start.x - end.x));
+  edgeBody->SetTransform(edgeBody->GetPosition(), angle);
 
-    //fixture must be the same length as distance between start and end
-    float distance = (end.x - start.x) * (end.x - start.x)
+  //fixture must be the same length as distance between start and end
+  float distance = (end.x - start.x) * (end.x - start.x)
                      + (end.y - start.y)*(end.y - start.y);
-    distance = sqrt(distance);
-    edgeShape.SetAsBox(distance/2, small_distance);
-    edgeBody->CreateFixture(&edgeShape, density);
+  distance = sqrt(distance);
+  edgeShape.SetAsBox(distance/2, small_distance);
+  edgeBody->CreateFixture(&edgeShape, density);
 
-    //Printing Edge occurs in the engine class
-  }
+  //Printing Edge occurs in the engine class
+}
 
 
 }
--- src/engine.cpp
+++ src/engine.cpp
@@ -19,90 +19,90 @@ const vector<double> green = myapp::Conversions::ToCinderRBG
     (myapp::Conversions::ColorChooser(19));
 
 
-  Engine::Engine(b2World &this_wrld) {
-    my_wrld = &this_wrld;
-    start_x = rand() % x_start_center + x_start_buffer;
-    end_x = rand() % x_end_center + x_end_buffer;
-    end_y = screen_size - block_buffer;
-    score = 0;
-  }
+Engine::Engine(b2World &this_wrld) {
+  my_wrld = &this_wrld;
+  start_x = rand() % x_start_center + x_start_buffer;
+  end_x = rand() % x_end_center + x_end_buffer;
+  end_y = screen_size - block_buffer;
+  score = 0;
+}
 
-  void Engine::Setup() {
-    CreateBalls(total_num_balls);
-    surfaces.CreateBox(my_wrld);
-  }
+void Engine::Setup() {
+  CreateBalls(total_num_balls);
+  surfaces.CreateBox(my_wrld);
+}
 
-  void Engine::CreateBalls(size_t number) {
-    for (size_t i = 0; i < number; i++) {
-      myapp::Ball next_ball;
-      next_ball.initialize(my_wrld, start_x);
-      all_balls.push_back(next_ball);
+void Engine::CreateBalls(size_t number) {
+  for (size_t i = 0; i < number; i++) {
+    myapp::Ball next_ball;
+    next_ball.initialize(my_wrld, start_x);
+    all_balls.push_back(next_ball);
+  }
+}
+
+void Engine::ActivateBalls() {
+  for (myapp::Ball ball: all_balls) {
+    ball.ActivateBall();
+  }
+}
+
+void Engine::CalculateScore() {
+  for (myapp::Ball ball : all_balls) {
+    if (ball.CheckFinished(cinder::vec2(end_x, end_y))) {
+      score++;
+      //"removes the body", moves it to be out of the way of other balls
+      ball.DiminishBody();
     }
   }
+}
 
-  void Engine::ActivateBalls() {
-    for (myapp::Ball ball: all_balls) {
-      ball.ActivateBall();
-    }
-  }
+void Engine::AddTempEdges(cinder::vec2 point) {
+  points.push_back(point);
+}
 
-  void Engine::CalculateScore() {
-    for (myapp::Ball ball : all_balls) {
-      if (ball.CheckFinished(cinder::vec2(end_x, end_y))) {
-        score++;
-        //"removes the body", moves it to be out of the way of other balls
-        ball.DiminishBody();
-      }
-    }
-  }
+vector<cinder::vec2> Engine::GetTempEdges() {
+  return points;
+}
 
-  void Engine::AddTempEdges(cinder::vec2 point) {
-    points.push_back(point);
-  }
+vector<myapp::Ball> Engine::GetAllBalls() {
+  return all_balls;
+}
 
-  vector<cinder::vec2> Engine::GetTempEdges() {
-    return points;
-  }
+myapp::Surfaces Engine::GetSurfaces() {
+  return surfaces;
+}
 
-  vector<myapp::Ball> Engine::GetAllBalls() {
-    return all_balls;
-  }
+size_t Engine::GetScore() {
+  CalculateScore();
+  return score;
+}
 
-  myapp::Surfaces Engine::GetSurfaces() {
-    return surfaces;
-  }
+void Engine::DrawEndBlock() {
+  cinder::gl::color(blue[0], blue[1], blue[2]);
 
-  size_t Engine::GetScore() {
-    CalculateScore();
-    return score;
-  }
-
-  void Engine::DrawEndBlock() {
-    cinder::gl::color(blue[0], blue[1], blue[2]);
-
-    cinder::vec2 end_point = cinder::vec2(end_x, end_y);
-    cinder::gl::drawSolidRect(cinder::Rectf(end_point.x - buffer,
+  cinder::vec2 end_point = cinder::vec2(end_x, end_y);
+  cinder::gl::drawSolidRect(cinder::Rectf(end_point.x - buffer,
                                             end_point.y - buffer,
                                             end_point.x + buffer,
                                             end_point.y + buffer));
-  }
+}
 
-  void Engine::DrawTempEdges() {
-    //if there are no drawn edges, no action should be taken
-    if (points.empty()) {
-      return;
-    }
-    cinder::gl::color(blue[0], blue[1], blue[2]);
-    size_t endpoint_radius = 2;
-
-    for (size_t i = 1; i < points.size(); i = i + 2) {
-      cinder::gl::drawLine(points[i], points[i - 1]);
-    }
-    for (cinder::vec2 point: points) {
-      cinder::gl::color(green[0], green[1], green[2]);
-      //Creates the click point, so user can see the edge start/end
-      cinder::gl::drawSolidCircle(point, endpoint_radius);
-    }
+void Engine::DrawTempEdges() {
+  //if there are no drawn edges, no action should be taken
+  if (points.empty()) {
+    return;
   }
+  cinder::gl::color(blue[0], blue[1], blue[2]);
+  size_t endpoint_radius = 2;
+
+  for (size_t i = 1; i < points.size(); i = i + 2) {
+    cinder::gl::drawLine(points[i], points[i - 1]);
+  }
+  for (cinder::vec2 point: points) {
+    cinder::gl::color(green[0], green[1], green[2]);
+    //Creates the click point, so user can see the edge start/end
+    cinder::gl::drawSolidCircle(point, endpoint_radius);
+  }
+}
 
 }
